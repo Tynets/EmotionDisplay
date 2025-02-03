ff3b8e4733d397e1eb80062227bb4d1e23e4fa81
Tynets <leshchova2004@gmail.com>
2025-02-04 00:32:33 +0100

    Debounce

--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -3,17 +3,14 @@
 #define BUTTON2 3
 const unsigned char cat1 = 'f';
 const unsigned char cat2 = 'e';
+int button1State;
+int button2State;
+int lastButton1State = LOW;
+int lastButton2State = LOW;
+unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
+unsigned long debounceDelay = 50;
 SoftwareSerial BTSerial(10, 9);
-
-volatile byte button1Released = false;
-void button1ReleasedInterrupt() {
-  button1Released = true;
-}
-
-volatile byte button2Released = false;
-void button2ReleasedInterrupt() {
-  button2Released = true;
-}
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,21 +18,32 @@ void setup() {
   Serial.begin(9600);
   pinMode(BUTTON1, INPUT);
   pinMode(BUTTON2, INPUT);
-  attachInterrupt(digitalPinToInterrupt(BUTTON1), button1ReleasedInterrupt, FALLING);
-  attachInterrupt(digitalPinToInterrupt(BUTTON2), button2ReleasedInterrupt, FALLING);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (button1Released) {
-    Serial.write('1');
-    button1Released = false;
-    BTSerial.write(cat1);
+  int reading1 = digitalRead(BUTTON1);
+  if (reading1 != lastButton1State) lastDebounceTime1 = millis();
+  if ((millis() - lastDebounceTime1) > debounceDelay) {
+    if (reading1 != button1State) {
+      button1State = reading1;
+      if (button1State == HIGH) {
+        Serial.write('1');
+        BTSerial.write(cat1);
+      }
+    }
   }
-  if (button2Released) {
-    Serial.write('2');
-    button2Released = false;
-    BTSerial.write(cat2);
+  lastButton1State = reading1;
+  int reading2 = digitalRead(BUTTON2);
+  if (reading2 != lastButton2State) lastDebounceTime2 = millis();
+  if ((millis() - lastDebounceTime2) > debounceDelay) {
+    if (reading2 != button2State) {
+      button2State = reading2;
+      if (button2State == HIGH) {
+        Serial.write('2');
+        BTSerial.write(cat2);
+      }
+    }
   }
-  delay(20);
+  lastButton2State = reading2;
 }
