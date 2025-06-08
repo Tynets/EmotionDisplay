6c9afffe2b328624e3c5e7c1f6e943c24ae125e8
Tynets <leshchova2004@gmail.com>
2025-06-08 22:06:27 +0200

    Finished previews

--- GridPaint/app/src/main/java/gp/gridpaint/CategoryElementSelection/CategoryElementSelectionActivity.kt
+++ GridPaint/app/src/main/java/gp/gridpaint/CategoryElementSelection/CategoryElementSelectionActivity.kt
@@ -1,8 +1,7 @@
 package gp.gridpaint.CategoryElementSelection
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.Color
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,8 +11,6 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import gp.gridpaint.GridPainter.MainActivity
 import gp.gridpaint.R
-import java.io.ByteArrayOutputStream
-
 
 class CategoryElementSelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,27 +22,17 @@ class CategoryElementSelectionActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val category = intent.getStringExtra("Category")
 
-        val colors = IntArray(256){Color.WHITE}
-        for (i in 100..150) colors[i] = Color.BLUE
-        colors[0] = Color.RED
-        colors[255] = Color.RED
-        val bitmap = Bitmap.createBitmap(colors, 16, 16, Bitmap.Config.RGB_565)
-
-        val category = intent.getIntExtra("Category", 0)
         val list = ArrayList<CategoryElementModel>()
         for (i in 1..10) {
-            list.add(CategoryElementModel(if (category == 0) bitmap else bitmap, if (category == 0) "Eyes $i" else "Mouth $i"))
+            val bitmap = BitmapFactory.decodeFile(this.filesDir.toString() + "/$category/$i.png")
+            list.add(CategoryElementModel(bitmap, "$i"))
         }
+
         val adapter = CategoryElementViewAdapter(list) { data ->
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("ID", data.title)
-            // bitmap
-            val stream = ByteArrayOutputStream()
-            data.image.compress(Bitmap.CompressFormat.PNG, 100, stream)
-            val byteArray = stream.toByteArray()
-            intent.putExtra("Bitmap", byteArray)
-            //
+            intent.putExtra("Element", category + "/" + data.title)
             startActivity(intent)
         }
         val recView: RecyclerView = findViewById(R.id.recycler_view_cat_el)
