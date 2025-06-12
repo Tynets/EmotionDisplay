e96a8ae333c6de0655cc4852f73e64b83b6776f8
Tynets <leshchova2004@gmail.com>
2025-06-12 18:52:58 +0200

    Send all data from Master

--- MasterSD/MasterSD.ino
+++ MasterSD/MasterSD.ino
@@ -1,10 +1,12 @@
 #include <Adafruit_NeoPixel.h>
 #include <SD.h>
-//#include <SoftwareSerial.h>
 #define MATRIX_PIN 8
 #define SD_PIN 9
 #define MOUTH_SIZE 10
 #define EYE_SIZE 10
+#define MOUTH_CATEGORY 0
+#define EYE_CATEGORY 1
+#define MESSAGE_HEADER 5
 #define BTRemote Serial1
 #define BTSmartphone Serial2
 
@@ -23,11 +25,45 @@ typedef struct Face {
   uint8_t currentEye;
 } Face;
 
-//SoftwareSerial BTSmartphone(12, 13);
 Adafruit_NeoPixel matrix(256, MATRIX_PIN, NEO_GRB + NEO_KHZ800);
 Face face;
 File file;
 File copyFile;
+
+void sendCategory(uint8_t category) {
+  uint8_t n = 0;
+  char categoryString[10];
+  memset(categoryString, '\0', sizeof(char) * 10);
+  if (category == MOUTH_CATEGORY) {
+    n = MOUTH_SIZE;
+    sprintf(categoryString, "mouthes");
+  }
+  else if (category == EYE_CATEGORY) {
+    n = EYE_SIZE;
+    sprintf(categoryString, "eyes");
+  }
+  else return;
+
+  for (uint8_t i = 1; i <= n; i++) {
+    char filename[50];
+    memset(filename, 0, 50 * sizeof(char));
+    sprintf(filename, "imgs/main/%s/%d.bin", categoryString, i);
+    file = SD.open(filename, FILE_READ);
+    uint32_t size = file.size();
+    byte* message = (byte*)(malloc(sizeof(Pixel) * size + sizeof(byte) * MESSAGE_HEADER));
+    file.readBytes((message + MESSAGE_HEADER), size);
+    file.close();
+    
+    message[0] = 'a';                   //command
+    message[1] = category;              // category
+    message[2] = i;                     // element
+    message[3] = size & 0xFF;           // file size LSB (mask for first 8 bits)
+    message[4] = (size >> 8) & 0xFF;    // file size MSB (remaining 8 bits)
+    BTSmartphone.write(message, MESSAGE_HEADER + size);
+
+    free(message);
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -45,13 +81,19 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (BTRemote.available()) {
     byte state = BTRemote.read();
-    Serial.write(state);
     if (state == 'e') face.currentEye = ++face.currentEye % EYE_SIZE;
     else if (state == 'f') face.currentMouth = ++face.currentMouth % MOUTH_SIZE;
     displayFace();
   }
   if (BTSmartphone.available()) {
-    Serial.print(BTSmartphone.read());
+    byte state = BTSmartphone.read();
+    if (state == 'a') {
+      sendCategory(MOUTH_CATEGORY);
+      sendCategory(EYE_CATEGORY);
+    }
+    else if (state == 'u') {
+      // update
+    }
   }
   delay(20);
 }
