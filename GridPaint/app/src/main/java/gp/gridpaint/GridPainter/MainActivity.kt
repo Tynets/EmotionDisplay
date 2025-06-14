3561d597d7067c3609b68ead208b340c41f6799c
Tynets <leshchova2004@gmail.com>
2025-06-14 19:01:31 +0200

    Added update message

--- GridPaint/app/src/main/java/gp/gridpaint/GridPainter/MainActivity.kt
+++ GridPaint/app/src/main/java/gp/gridpaint/GridPainter/MainActivity.kt
@@ -8,14 +8,24 @@ import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.Button
 import android.widget.GridView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.blue
 import androidx.core.graphics.get
+import androidx.core.graphics.green
+import androidx.core.graphics.red
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import gp.gridpaint.CategoryElementSelection.CategoryElementSelectionActivity
 import gp.gridpaint.R
 import java.io.FileOutputStream
+
+fun Int.toSignedByte() : Byte {
+    return if (this > 255) throw IllegalArgumentException("Must be lower that 256")
+    else if (this < 128) this.toByte()
+    else return (-(this.inv() + 1)).toByte()
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,8 +46,8 @@ class MainActivity : AppCompatActivity() {
         for (i in 0..255) {
             val x: Int = i % 16
             val y: Int = i / 16
-            var pos: Int = i
-            if (y % 2 == 0) pos = i + (15 - 2 * x)
+            var pos: Int = 255 - i
+            if (y % 2 == 1) pos = 255 - i - (15 - 2 * x)
             list.add(PixelModel(bitmap[x, y], pos))
         }
         val adapter = GridViewAdapter(this, list)
@@ -79,13 +89,41 @@ class MainActivity : AppCompatActivity() {
         val updateButton: Button = findViewById(R.id.update_button)
         updateButton.setOnClickListener {
             val colors = IntArray(256)
-            for (i in 0..255) colors[i] = list[i].color
+            val pixelsToSend = arrayListOf<PixelModel>()
+            for (i in 0..255) {
+                colors[i] = list[i].color
+                if (list[i].color != Color.BLACK) pixelsToSend.add(list[i])
+            }
             val bitmapUpdate = Bitmap.createBitmap(colors, 16, 16, Bitmap.Config.RGB_565)
             val stream = FileOutputStream(this.filesDir.toString() + "/$element.png")
             bitmapUpdate.compress(Bitmap.CompressFormat.PNG, 100, stream)
             stream.flush()
             stream.close()
+
+            val size: Int = pixelsToSend.size * 5
+            val message = ByteArray(5 + size)
+            val elementSplit  = element!!.split("/")
+            message[0] = 'u'.code.toSignedByte()
+            message[1] = when (elementSplit[0]){
+                "Eyes" -> 1.toSignedByte()
+                "Mouths" -> 0.toSignedByte()
+                else -> return@setOnClickListener
+            }
+            message[2] = elementSplit[1].toInt().toSignedByte()
+            message[3] = (size and 0xFF).toSignedByte()
+            message[4] = (size shr 8).toSignedByte()
+            for (i in 5..size step 5) {
+                val pixel = pixelsToSend.removeAt(0)
+                message[i] = (pixel.pos and 0xFF).toSignedByte()
+                message[i + 1] = (pixel.pos shr 8).toSignedByte()
+                message[i + 2] = pixel.color.red.toSignedByte()
+                message[i + 3] = pixel.color.green.toSignedByte()
+                message[i + 4] = pixel.color.blue.toSignedByte()
+            }
+            // send to board
+            Toast.makeText(this, "${elementSplit[0]} updated", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, CategoryElementSelectionActivity::class.java)
+            intent.putExtra("Category", elementSplit[0])
             startActivity(intent)
         }
     }
